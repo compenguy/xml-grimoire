5f8eeb1feb60627c73c933e3e73746bf8e97b159
Will Page <compenguy@gmail.com>
2018-11-23 09:50:49 -0800

    Complete the test of lexing into a token tree

--- src/token.rs
+++ src/token.rs
@@ -35,10 +35,43 @@ mod tests {
     #[test]
     fn test_recursive() {
         let content = "<doc></doc>";
-        let _doc_token = match XmlLexer1_0::parse(Rule::document, content) {
+        // document
+        // └ element
+        //   ├ STag
+        //   │ └ Name
+        //   └ ETag
+        //     └ Name
+        let doc_token = match XmlLexer1_0::parse(Rule::document, content) {
             Err(e) => panic!("Lexing the XML content failed: {}", e),
             Ok(mut p) => Token::from(p.next().unwrap()),
         };
-        /* TODO: actually verify the structure */
+        assert_eq!(doc_token.content, "<doc></doc>");
+        assert_eq!(doc_token.token_type, Rule::document);
+        assert_eq!(doc_token.children.len(), 1);
+
+        let element_token = &doc_token.children[0];
+        assert_eq!(element_token.content, "<doc></doc>");
+        assert_eq!(element_token.token_type, Rule::element);
+        assert_eq!(element_token.children.len(), 2);
+
+        let start_tag_token = &element_token.children[0];
+        assert_eq!(start_tag_token.content, "<doc>");
+        assert_eq!(start_tag_token.token_type, Rule::STag);
+        assert_eq!(start_tag_token.children.len(), 1);
+
+        let start_name_token = &start_tag_token.children[0];
+        assert_eq!(start_name_token.content, "doc");
+        assert_eq!(start_name_token.token_type, Rule::Name);
+        assert!(start_name_token.children.is_empty());
+
+        let end_tag_token = &element_token.children[1];
+        assert_eq!(end_tag_token.content, "</doc>");
+        assert_eq!(end_tag_token.token_type, Rule::ETag);
+        assert_eq!(end_tag_token.children.len(), 1);
+
+        let end_name_token = &end_tag_token.children[0];
+        assert_eq!(end_name_token.content, "doc");
+        assert_eq!(end_name_token.token_type, Rule::Name);
+        assert!(end_name_token.children.is_empty());
     }
 }
