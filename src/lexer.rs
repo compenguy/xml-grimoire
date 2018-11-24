98fc7c41da4db79c831aecf1b0d8e3273046fae5
Will Page <compenguy@gmail.com>
2018-11-24 10:44:39 -0800

    Updating unit tests to be more tolerant of changes to emitted tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -6,7 +6,7 @@ use token::Token;
 
 #[derive(Parser)]
 #[grammar = "xml1_0.pest"]
-struct XmlLexer1_0;
+pub(crate) struct XmlLexer1_0;
 
 pub use lexer::Rule as TokenType;
 
@@ -35,31 +35,44 @@ mod tests {
         };
         assert_eq!(doc_token.content, "<doc></doc>");
         assert_eq!(doc_token.token_type, TokenType::document);
-        assert_eq!(doc_token.children.len(), 1);
 
-        let element_token = &doc_token.children[0];
+        let mut doc_token_children = doc_token.children.iter();
+        let element_token = doc_token_children
+            .find(|t| t.token_type == TokenType::element)
+            .expect("Unable to find element where it was expected");
         assert_eq!(element_token.content, "<doc></doc>");
-        assert_eq!(element_token.token_type, TokenType::element);
-        assert_eq!(element_token.children.len(), 2);
 
-        let start_tag_token = &element_token.children[0];
+        let mut element_token_children = element_token.children.iter();
+        let start_tag_token = element_token_children
+            .find(|t| t.token_type == TokenType::STag)
+            .expect("Unable to find STag where it was expected");
         assert_eq!(start_tag_token.content, "<doc>");
-        assert_eq!(start_tag_token.token_type, TokenType::STag);
-        assert_eq!(start_tag_token.children.len(), 1);
 
-        let start_name_token = &start_tag_token.children[0];
+        let mut start_tag_children = start_tag_token.children.iter();
+        let start_name_token = start_tag_children
+            .find(|t| t.token_type == TokenType::Name)
+            .expect("Unable to find Name where it was expected");
         assert_eq!(start_name_token.content, "doc");
-        assert_eq!(start_name_token.token_type, TokenType::Name);
-        assert!(start_name_token.children.is_empty());
 
-        let end_tag_token = &element_token.children[1];
+        // Assert that the start name token has no content-bearing children
+        for token in &start_name_token.children {
+            assert!(token.content.is_empty());
+        }
+
+        let end_tag_token = element_token_children
+            .find(|t| t.token_type == TokenType::ETag)
+            .expect("Unable to find ETag where it was expected");
         assert_eq!(end_tag_token.content, "</doc>");
-        assert_eq!(end_tag_token.token_type, TokenType::ETag);
-        assert_eq!(end_tag_token.children.len(), 1);
 
-        let end_name_token = &end_tag_token.children[0];
+        let mut end_tag_children = end_tag_token.children.iter();
+        let end_name_token = end_tag_children
+            .find(|t| t.token_type == TokenType::Name)
+            .expect("Unable to find Name where it was expected");
         assert_eq!(end_name_token.content, "doc");
-        assert_eq!(end_name_token.token_type, TokenType::Name);
-        assert!(end_name_token.children.is_empty());
+
+        // Assert that the end name token has no content-bearing children
+        for token in &end_name_token.children {
+            assert!(token.content.is_empty());
+        }
     }
 }
